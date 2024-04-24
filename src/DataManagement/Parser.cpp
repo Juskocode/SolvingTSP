1d1f9fafb41827f396722e34def36e1d8d7bc56b
Freitas <adavidaf2002@gmail.com>
2024-04-24 12:37:55 +0100

    Parser class sketch

---
+++ src/DataManagement/Parser.cpp
@@ -0,0 +1,24 @@
+
+#include "Parser.h"
+
+Parser::Parser(const std::string &path, const std::string &Filename)
+{
+    this->path = path + "/";
+    this->fileName = Filename;
+}
+
+void Parser::readNodes()
+{
+
+}
+
+void Parser::readEdges()
+{
+
+}
+
+void Parser::readOnlyEdges(int N)
+{
+
+}
+
