1c811f59fb995bea93cafbca21bc5443ffc33067
Freitas <adavidaf2002@gmail.com>
2024-04-24 11:59:29 +0100

    Graph.h struct implementation

--- DataStructs/Graph.h
+++ DataStructs/Graph.h
@@ -59,11 +59,58 @@ public:
     std::vector<Edge*> adj;  //! adjacency list.
 
     /**
-     * @brief Overloaded less than operator for comparing nodes.
+     * @brief "<" Operator overloading
      * @param node The Node object to be compared.
      * @return True if the current node is less than the given node, False otherwise.
      */
     bool operator<(Node& node) const;
 };
 
+/**
+ * @class Graph
+ * @brief Represents a graph.
+ */
+class Graph
+{
+public:
+    /**
+     * @brief Default constructor for Graph.
+     */
+    Graph();
+
+    /**
+     * @brief Constructs a Graph object with the specified number of nodes.
+     * @param V The number of nodes in the graph.
+     */
+    explicit Graph(int V);
+
+    /**
+     * @brief Destructor for Graph.
+     */
+    ~Graph();
+
+    /**
+     * @brief Calculates the distance between two nodes.
+     * @param src The index of the source node.
+     * @param dest The index of the destination node.
+     * @note If the there is and edge between those nodes, it return the distance
+     * otherwise it computes the distance using Haversine formula
+     * @return The distance between the two nodes.
+     */
+    double findDistance(int src, int dest);
+
+    /**
+     * Calculates the distance between two airports given the latitude and longitude, using haversine formula\n \n
+     *
+     * @param lat1 - latitude of airport 1
+     * @param lon1 - longitude of airport 1
+     * @param lat2 - latitude of airport 2
+     * @param lon2 - longitude of airport 2
+     * @return distance between the two airports
+     */
+    static double haversineDistanceGeneric(double lat1, double lon1, double lat2, double lon2);
+
+    int N;                          //! Number of the nodes in the graph
+    std::vector<Node*> nodes;       //! Vector of the node of the graph
+};
 #endif
