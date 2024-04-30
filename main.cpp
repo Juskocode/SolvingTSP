b7d2e5883dae7758228b7f33c27cf7222b386803
Freitas <adavidaf2002@gmail.com>
2024-04-30 16:05:00 +0100

    Organized main into testing!

--- main.cpp
+++ main.cpp
@@ -2,6 +2,46 @@
 #include "src/DataManagement/Parser.h"
 
 using namespace std;
+
+void printGraph(const Graph &g1)
+{
+    for (auto n : g1.nodes)
+    {
+        cout << n->id << " -> ";
+        for (auto i : n->adj)
+            cout << "|" << i->dest << ", " << i->weight << "| ";
+        cout << '\n';
+    }
+    cout << '\n';
+}
+
+void printMstGraph(const Graph &g1)
+{
+    for (int i = 0; i < g1.N; i++)
+    {
+        auto adj = g1.mst[i];
+        for (const auto & e : adj)
+            cout << i << ", " << e << " ";
+        cout << endl;
+    }
+}
+
+void testBackTrackNaive(Graph g, clock_t &start, clock_t &end)
+{
+    start = clock();
+    cout << "TSP tour cost: " << g.tspBackTrackingNaive() << endl;
+    end = clock();
+    cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
+}
+
+void testBackTrackHeldKarp(Graph g, clock_t &start, clock_t &end)
+{
+    start = clock();
+    cout << "TSP tour cost: " << g.tspBackTrackingHeldKarp() << endl;
+    end = clock();
+    cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
+}
+
 int main()
 {
     //TODO ORGANIZE this shitty main into testing
@@ -11,87 +51,29 @@ int main()
     Parser p;
     clock_t start, end;
 
-    cout << "BackTrack Algorithm:\n";
     p.readOnlyEdges(g1, "../Data/Toy-Graphs/shipping.csv", 14);
-    cout << "shipping" << endl;
-    for (auto n : g1.nodes)
-    {
-        cout << n->id << " -> ";
-        for (auto i : n->adj)
-            cout << "|" << i->dest << ", " << i->weight << "| ";
-        cout << '\n';
-    }
     p.readOnlyEdges(g2, "../Data/Toy-Graphs/stadiums.csv", 11);
-    cout << "stadiums" << endl;
-    for (auto n : g2.nodes)
-    {
-        cout << n->id << " -> ";
-        for (auto i : n->adj)
-            cout << "|" << i->dest << ", " << i->weight << "| ";
-        cout << '\n';
-    }
     p.readOnlyEdges(g3, "../Data/Toy-Graphs/tourism.csv", 5);
-    cout << "tourism" << endl;
-    for (auto n : g3.nodes)
-    {
-        cout << n->id << " -> ";
-        for (auto i : n->adj)
-            cout << "|" << i->dest << ", " << i->weight << "| ";
-        cout << '\n';
-    }
+
+    cout << "g1 :" << endl;
+    printGraph(g1);
+    cout << "g2 :" << endl;
+    printGraph(g2);
+    cout << "g3 :" << endl;
+    printGraph(g3);
 
     start = clock();
-    cout << "Shipping: " << g1.tspBackTrackingHeldKarp() << endl;
-    end = clock();
-    cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
 
-
-    //start = clock();
-    //cout << "Stadiums Naive Backtracking: \ncost: " << g2.tspBackTrackingNaive() << endl;
-    //end = clock();
-    //cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
-    start = clock();
-    cout << "Stadiums Help-Karp: \ncost: " << g2.tspBackTrackingHeldKarp() << endl;
-    end = clock();
-    cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
-
-    start = clock();
-    cout << "Tourism Naive Backtracking: \ncost: " << g3.tspBackTrackingNaive() + 1 << endl;
-    end = clock();
-    cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
-    start = clock();
-    cout << "Tourism Held-Karp: \ncost: " << g3.tspBackTrackingHeldKarp() << endl;
-    end = clock();
-    cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
-    cout << endl;
-
-    cout << "mst's edges: " << endl;
+    cout << "mst's edges g1: " << endl;
     g1.buildMst(0);
-    for (int i = 0; i < g1.N; i++)
-    {
-        auto adj = g1.mst[i];
-        for (const auto & e : adj)
-            cout << i << ", " << e << " ";
-        cout << endl;
-    }
-    cout << endl;
+    printMstGraph(g1);
+    cout << endl;    cout << "mst's edges g2: " << endl;
     g2.buildMst(0);
-    for (int i = 0; i < g2.N; i++)
-    {
-        auto adj = g2.mst[i];
-        for (const auto & e : adj)
-            cout << i << "," << e << " ";
-        cout << endl;
-    }
+    printMstGraph(g2);
     cout << endl;
+    cout << "mst's edges g3: " << endl;
     g3.buildMst(0);
-    for (int i = 0; i < g3.N; i++)
-    {
-        auto adj = g3.mst[i];
-        for (const auto & e : adj)
-            cout << i << "," << e << " ";
-        cout << endl;
-    }
+    printMstGraph(g3);
     cout << endl;
     return 0;
 }
