75919491cc6c26c2525259f6940e284c38e5f765
juskocode <adavidaf2002@gmail.com>
2024-05-16 02:28:55 +0100

    Christofides testing

--- main.cpp
+++ main.cpp
@@ -50,10 +50,63 @@ void testBackTrackHeldKarp(const Parser &p, clock_t &start, clock_t &end)
         string path = "../Data/Toy-Graphs/" + fileName + ".csv";
         p.readOnlyEdges(g, path, nodes);
         start = clock();
-        cout << "BackTracking Approx " << nodes << ": " << g.tspBackTrackingHeldKarp() << " m" << endl;
+        cout << "BackTracking results " << nodes << ": " << g.tspBackTrackingHeldKarp() << " m" << endl;
         end = clock();
         cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
         //cout << "lowerBound : " << g.OneTreeLowerBound() << " m" << endl;
+    }
+}
+
+void testChristofidesToyGraphs(const Parser &p, clock_t &start, clock_t &end)
+{
+    vector<pair<string, int>> files = {{"shipping", 14}, {"stadiums", 11}, {"tourism", 5}};
+    for (const auto &[fileName, nodes]: files)
+    {
+        Graph g;
+        string path = "../Data/Toy-Graphs/" + fileName + ".csv";
+        p.readOnlyEdges(g, path, nodes);
+        start = clock();
+        cout << "Christofides Approx " << nodes << ": " << g.tspChristofides(true) << " m" << endl;
+        end = clock();
+        cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
+        //cout << "lowerBound : " << g.OneTreeLowerBound() << " m" << endl;
+    }
+}
+
+void testChristofidesExtraFullyConnectGraphs(const Parser &p, clock_t start, clock_t end)
+{
+    vector<int> files = {25, 50, 75, 100, 200, 300, 400, 500, 600, 700, 800, 900};
+    double cost = 0.0;
+    for (int n: files)
+    {
+        Graph g;
+        string path = "../Data/Extra_Fully_Connected_Graphs/edges_" + to_string(n) + ".csv";
+        p.readOnlyEdges(g, path, n);
+        start = clock();
+        cost = g.tspChristofides(true);
+        end = clock();
+        cout << "N.N " << n << ": " << cost / 1e3 << " km" << endl;
+        cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
+        cout << "Performance of cost : " << cost / g.OneTreeLowerBound(true) << endl;
+    }
+}
+
+void testChristofidesRealGraphs(const Parser &p, clock_t start, clock_t end)
+{
+    double cost = 0.0;
+    for (int i = 1; i < 4; i++)
+    {
+        Graph g;
+        string path = "../Data/Real_world_Graphs/graph" + to_string(i);
+        p.readNodes(g, path + "/nodes.csv");
+        p.readEdges(g, path + "/edges.csv");
+
+        start = clock();
+        cost = g.tspChristofides(false);
+        end = clock();
+        cout << "Triangle Approx Real Graph" << i << ": " << cost / 1e3 << " km" << endl;
+        cout << "Time: " << (double) (end - start) / CLOCKS_PER_SEC << endl;
+        cout << "Performance of cost : " << cost / g.OneTreeLowerBound(false) << endl;
     }
 }
 
@@ -140,10 +193,11 @@ int main()
 
     //testBackTrackHeldKarp(p, start, end);
     //cout << "-----------------------------" << endl;
+    testChristofidesExtraFullyConnectGraphs(p, start, end);
     //testTriangularExtraFullyConnectedGraphs(p, start, end);
     //cout << "-----------------------------" << endl;
     //testTriangularRealGraphs(p, start, end);
-    testNearestNeighborExtraFullyConnectGraphs(p, start, end);
+    //testNearestNeighborExtraFullyConnectGraphs(p, start, end);
 
     return 0;
 }
